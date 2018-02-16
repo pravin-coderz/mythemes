e658fb4ae8fc3d3f6c0f3c625382957f59ec9976
pravinrcp <pravinrcp3@gmail.com>
2018-02-16 11:14:07 +0530

    delete some files

--- wp-content/themes/themes/index.php
+++ wp-content/themes/themes/index.php
@@ -4,7 +4,15 @@ Template Name: Home Page
 ****************************/
 get_header(); 
 
+<<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
 
+</body>
+</html>>
 
 
 get_footer(); ?>
